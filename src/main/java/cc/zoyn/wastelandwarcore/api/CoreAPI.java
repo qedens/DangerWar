5eb8157ce791dabc9dc1973930e8f2c6109a6c48
Zoyn <602723113@qq.com>
2017-12-30 20:04:02 +0800

    update: CoreAPI

--- src/main/java/cc/zoyn/wastelandwarcore/api/CoreAPI.java
+++ src/main/java/cc/zoyn/wastelandwarcore/api/CoreAPI.java
@@ -1,9 +1,13 @@
 package cc.zoyn.wastelandwarcore.api;
 
 import cc.zoyn.wastelandwarcore.manager.*;
+import cc.zoyn.wastelandwarcore.module.town.Town;
 import cc.zoyn.wastelandwarcore.util.ActionBarUtils;
 import cc.zoyn.wastelandwarcore.util.TitleUtils;
+import org.apache.commons.lang3.Validate;
 import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
 
 /**
  * 核心API
@@ -64,6 +68,50 @@ public class CoreAPI {
 
     public static void sendActionbar(Player player, String msg) {
         ActionBarUtils.sendBar(player, msg);
+    }
+
+    /**
+     * 判断两个玩家是否为同一阵营
+     *
+     * @param playerAName 玩家A的名字
+     * @param playerBName 玩家B的名字
+     * @return {@link Boolean}
+     */
+    public static boolean isFriendly(String playerAName, String playerBName) {
+        Town townA = getTownManager().getTownByMember(playerAName);
+        Town townB = getTownManager().getTownByMember(playerBName);
+        return townA.getName().equals(townB.getName());
+    }
+
+    /**
+     * 判断两个玩家是否为同一阵营
+     *
+     * @param playerA 玩家A
+     * @param playerB 玩家B
+     * @return {@link Boolean}
+     */
+    public static boolean isFriendly(@Nonnull Player playerA, @Nonnull Player playerB) {
+        return isFriendly(playerA.getName(), playerB.getName());
+    }
+
+    /**
+     * 获取城镇的 盟主 名字
+     *
+     * @param town 城镇对象
+     * @return 盟主的名字
+     */
+    public static String getTownOwnerName(Town town) {
+        return Validate.notNull(town.getOwner());
+    }
+
+    /**
+     * 获取城镇的 盟主 名字
+     *
+     * @param townName 城镇名
+     * @return 盟主的名字
+     */
+    public static String getTownOwnerName(String townName) {
+        return getTownOwnerName(getTownManager().getTownByName(townName));
     }
 
 }
